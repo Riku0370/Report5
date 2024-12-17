ad58cb5f4b10fcd6a53db7f6ee8dd51bff504e4c
Riku Tamaki <e245734@ie.u-ryukyu.ac.jp>
2024-12-17 22:41:25 +0900

    Report5

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        String str = null;
-        System.out.println(str.length());
+        try {
+            String str = null;
+            System.out.println(str.length());
+            
+        } catch (NullPointerException n) {
+            System.out.println("NullPointerExceptionが発生した");
+            System.out.println(n.getMessage());
+        }
     }
 }
