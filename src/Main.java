742917969e09bb4437b1653b1f5117c9deb8255e
Riku Tamaki <e245734@ie.u-ryukyu.ac.jp>
2024-12-17 23:19:26 +0900

    step3

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        try {
-            String str = null;
-            System.out.println(str.length());
-            
-        } catch (NullPointerException n) {
-            System.out.println("NullPointerExceptionが発生した");
-            System.out.println(n.getMessage());
-        }
+    String str = "壱百満";
+    int value = Integer.parseInt(str);
     }
 }
